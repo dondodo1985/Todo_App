99d1b62b255661e3318c014152a6de2508a4a728
dondodo1985 <72082423+dondodo1985@users.noreply.github.com>
2020-12-25 19:08:11 -0500

    First commit

---
+++ todo/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from .models import TodoItem
+# Create your views here.
+
+
+def todoView(request):
+    all_items = TodoItem.objects.all()
+    return render(request,'todo.html', {'all_items':all_items})
+
+
+def addTodo(request):
+    content = request.POST['content']
+    new_item = TodoItem(content= content)
+    new_item.save()
+    return HttpResponseRedirect('/todo/')
+
+def deleteTodo(request, todo_id):
+    delete_item =TodoItem.objects.get(id=todo_id)
+    delete_item.delete()
+    return HttpResponseRedirect('/todo/')
